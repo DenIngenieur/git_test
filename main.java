9414af88916a447257016f5ffc0209f5d7bf769c
Rudy <44776187+DenIngenieur@users.noreply.github.com>
2021-03-10 20:17:55 +0100

    Initial commit

---
+++ main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) {
+        int deler, teller = 0, posGetal = 0;
+         
+        Scanner invoer = new Scanner(System.in);
+
+        do {
+            System.out.print("Geef een strikt positieve deler in: ");
+            deler = invoer.nextInt();
+        } while (deler <= 0);
+        
+        do {
+            if(posGetal > 0){
+                if (posGetal%deler == 0){
+                    teller++;
+                }
+            }
+            System.out.print("Geef een strikt postief getal in (of stop met -1): ");
+            posGetal = invoer.nextInt();
+        } while (posGetal != -1);
+        
+        invoer.close();
+
+        if (teller == 1){
+            System.out.printf("Er is %d getal deelbaar door %d%n", teller, deler);
+        }
+        else {
+            System.out.printf("Er zijn %d getallen deelbaar door %d%n", teller, deler);
+        }
+    }
+}
